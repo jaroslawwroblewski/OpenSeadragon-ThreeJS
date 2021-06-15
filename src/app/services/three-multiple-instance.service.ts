fd54603994eda45d1777cfbfafbd61764885e9b3
jaroslawwroblewski <j.wroblewski.net@gmail.com>
2021-06-15 12:55:41 +0200

    set-camera-threejs

--- src/app/services/three-multiple-instance.service.ts
+++ src/app/services/three-multiple-instance.service.ts
@@ -1,9 +1,10 @@
 import { ElementRef, Injectable, NgZone } from '@angular/core';
-import { Scene, WebGLRenderer } from 'three';
+import { Scene, OrthographicCamera, WebGLRenderer } from 'three';
 
 @Injectable({ providedIn: 'root' })
 export class ThreeMultipleInstanceService {
   private renderer: WebGLRenderer;
+  private camera: OrthographicCamera;
 
   constructor(private ngZone: NgZone) {}
 
@@ -13,11 +14,35 @@ export class ThreeMultipleInstanceService {
     bounds: any
   ): void {
     const scene = new Scene();
-
     this.setCamera(bounds);
   }
 
-  private setCamera(bounds) {}
+  private setCamera(bounds) {
+    const { x, y, width, height } = bounds;
+    this.camera
+      ? this.updateCameraSize(this.camera, width, height)
+      : (this.camera = new OrthographicCamera(
+          0,
+          width,
+          0,
+          height,
+          0,
+          20
+        ));
+
+    this.camera.position.set(x, y, 1);
+    this.camera.clearViewOffset();
+    this.camera.updateProjectionMatrix();
+  }
+
+  private updateCameraSize(
+    camera: OrthographicCamera,
+    width: number,
+    height: number
+  ): void {
+    camera.right = width;
+    camera.bottom = height;
+  }
 
   // public render(scene): void {
   //   if (!scene) return;
