60f55216774a3662ace72b2957aedb3f0e66b146
jaroslawwroblewski <j.wroblewski.net@gmail.com>
2021-06-17 14:54:09 +0200

    fix-problem-with-canvas

--- src/app/core/threejs.settings.ts
+++ src/app/core/threejs.settings.ts
@@ -1,7 +1,7 @@
 import { InstancesType } from '../enums/threejs.enum';
 
 export const THREEJS_INSTANES = InstancesType.Single;
-export const AMOUNT_OF_VIEWPORTS = 6;
+export const AMOUNT_OF_VIEWPORTS = 4;
 export const AMOUNT_OF_ANNOTATIONS = 100;
 export const CIRCLE_SIZE = 200;
 export const COLORS = [0x00ff00, 0xe50d9c, 0x2247c2, 0xffff00];
