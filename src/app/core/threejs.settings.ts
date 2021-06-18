0404f15e03b7579f15456595cab2e98bea480301
jaroslawwroblewski <j.wroblewski.net@gmail.com>
2021-06-18 12:57:43 +0200

    reduce annotations

--- src/app/core/threejs.settings.ts
+++ src/app/core/threejs.settings.ts
@@ -8,7 +8,7 @@ import { InstancesType } from '../enums/threejs.enum';
 export const THREEJS_INSTANES = InstancesType.Multiple;
 
 export const AMOUNT_OF_VIEWPORTS = 6;
-export const AMOUNT_OF_ANNOTATIONS = 10000;
+export const AMOUNT_OF_ANNOTATIONS = 5000;
 export const AMOUNT_OF_GEOMETRY_IN_ANNOTATION = 200;
 export const CIRCLE_SIZE = 200;
 export const COLORS = [0x00ff00, 0xe50d9c, 0x2247c2, 0xffff00];
